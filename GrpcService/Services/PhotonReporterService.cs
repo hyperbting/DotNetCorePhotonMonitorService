9d2f12c81f0d6e5ab1ea328844f058f41f188461
thomas.kao <thomas.kao@xrspace.io>
2021-10-14 15:53:25 +0800

    feat: general gRPC message defines

---
+++ GrpcService/Services/PhotonReporterService.cs
@@ -0,0 +1,26 @@
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcService1
+{
+    //public class PhotonReporterService : photonroom.photonroombase
+    //{
+    //    private readonly ILogger<PhotonReporterService> _logger;
+    //    public PhotonReporterService(ILogger<PhotonReporterService> logger)
+    //    {
+    //        _logger = logger;
+    //    }
+
+    //    public override Task<RoomListReply> RequestRoomList(RoomListRequest request, ServerCallContext context)
+    //    {
+    //        return Task.FromResult(new HelloReply
+    //        {
+    //            Message = "Hello " + request.Name
+    //        });
+    //    }
+    //}
+}
