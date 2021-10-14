6b94b3895e4fb23fa95a37669c22f58c7a0e765a
thomas.kao <thomas.kao@xrspace.io>
2021-10-14 16:02:02 +0800

    feat: general gRPC Scaffold

--- GrpcService/Services/PhotonReporterService.cs
+++ GrpcService/Services/PhotonReporterService.cs
@@ -7,20 +7,20 @@ using System.Threading.Tasks;
 
 namespace GrpcService1
 {
-    //public class PhotonReporterService : photonroom.photonroombase
-    //{
-    //    private readonly ILogger<PhotonReporterService> _logger;
-    //    public PhotonReporterService(ILogger<PhotonReporterService> logger)
-    //    {
-    //        _logger = logger;
-    //    }
+    public class PhotonReporterService : ReportPhotoRoom.ReportPhotoRoomBase
+    {
+        private readonly ILogger<PhotonReporterService> _logger;
+        public PhotonReporterService(ILogger<PhotonReporterService> logger)
+        {
+            _logger = logger;
+        }
 
-    //    public override Task<RoomListReply> RequestRoomList(RoomListRequest request, ServerCallContext context)
-    //    {
-    //        return Task.FromResult(new HelloReply
-    //        {
-    //            Message = "Hello " + request.Name
-    //        });
-    //    }
-    //}
+        public override Task<RoomListReply> RequestRoomList(RoomListRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new RoomListReply
+            {
+                //Message = "Hello " + request.Name
+            });
+        }
+    }
 }
